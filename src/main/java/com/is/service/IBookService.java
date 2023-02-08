adfe60043013d1b9686f374c30e7b6bcb2da7a5a
ivansnow02 <ivansnow02@gmail.com>
2023-02-08 14:41:57 +0800

    表现层

--- src/main/java/com/is/service/IBookService.java
+++ src/main/java/com/is/service/IBookService.java
@@ -1,9 +1,11 @@
 package com.is.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.is.domain.Book;
 import org.springframework.stereotype.Service;
 
 
 public interface IBookService extends IService<Book> {
+    public IPage<Book> getPage(int currentPage, int pageSize);
 }
--- src/main/java/com/is/service/impl/BookServiceImpl.java
+++ src/main/java/com/is/service/impl/BookServiceImpl.java
@@ -1,11 +1,25 @@
 package com.is.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.is.dao.BookDao;
 import com.is.domain.Book;
 import com.is.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBookService {
+
+    private final BookDao bookDao;
+    @Autowired
+    public BookServiceImpl(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    public IPage<Book> getPage(int currentPage, int pageSize) {
+        IPage<Book> page = new Page<>(currentPage, pageSize);
+        return bookDao.selectPage(page, null);
+    }
 }
